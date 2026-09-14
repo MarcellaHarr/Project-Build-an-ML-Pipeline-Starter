d5411d0237fc6c0f79065df4da2c1b1c17889cca
Marcella <marcellalharris@yahoo.com>
2026-09-14 19:14:42 -0400

    step 3: wrote a few tests and added it to the ML pipeline

--- main.py
+++ main.py
@@ -72,7 +72,19 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            #== use the data_check component ==
+            _ = mlflow.run(
+                "src/data_check",
+                "main",
+                env_manager="conda",
+                parameters = {
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                }
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -87,3 +87,33 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+#== check the size of the dataset ==
+def test_row_count(data: pd.DataFrame) -> None:
+    """
+    Test if the number of rows in the DataFrame is within a reasonable range.
+
+    Args:
+        data: Input DataFrame to test
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+#== check the price range of the dataset ==
+def test_price_range(
+        data: pd.DataFrame, 
+        min_price: float, 
+        max_price: float
+) -> None:
+    """
+    Test if the price values in the DataFrame are 
+    within the expected [min_price, max_price] range.
+
+    Args:
+        data: Current dataset to test
+        min_price: Minimum accepted price
+        max_price: Maximum accepted price
+    """
+    assert data["price"].between(
+        min_price,
+        max_price
+    ).all()
